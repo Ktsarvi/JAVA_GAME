1bfdf8501a42d58b627c4b20249cb1ce1c4a260d
Said Aghamaliyev <digitalwarior666@gmail.com>
2026-05-08 20:35:38 +0400

    Create Item.java

---
+++ items/Item.java
@@ -0,0 +1,38 @@
+package items;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public abstract class Item {
+    protected String name;                 // Item name (Apple, Key, Potion etc.)
+    protected BufferedImage sprite;        // Sprite(icon) for UI and inventory
+    protected boolean consumable = false;  // If true item will be removed after use
+    
+    public Item(String name) {
+        this.name = name;
+    }
+
+    // Called when player uses the item
+    public abstract void use();
+
+    // Draw item in inventory or UI
+    public void draw(Graphics2D g2, int x, int y, int size) {
+        if (icon != null) {
+            g2.drawImage(icon, x, y, size, size, null);
+        }
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public BufferedImage getIcon() {
+        return icon;
+    }
+
+    // Boolean checkers
+    public boolean isConsumable() {
+        return consumable;
+    }
+}
