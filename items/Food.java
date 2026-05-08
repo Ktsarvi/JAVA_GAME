f74dd9b6c8f72a7490f98904ae18c0ce0e513121
Said Aghamaliyev <digitalwarior666@gmail.com>
2026-05-08 20:47:47 +0400

    Update Food.java

--- items/Food.java
+++ items/Food.java
@@ -2,10 +2,12 @@ package items;
 
 public abstract class Food extends Item {
     protected int healAmount;  // How much HP it will heal player
+    protected bool speedBoost;    // If food gave speed boost to player
 
-    public Food(String name, int healAmount) {
+    public Food(String name, int healAmount, bool speedBoost) {
         super(name);
         this.healAmount = healAmount;
+        this.speedBoost = speedBoost;
 
         // Food is usually consumed after use
         this.consumable = true;
@@ -20,5 +22,9 @@ public abstract class Food extends Item {
     // Getters
     public int getHealAmount() {
         return healAmount;
+    }
+
+    public bool getSpeedBoost() {
+        return speedBoost;
     }
 }
